818e5c0ce6793db068f2a4327b2cd58ce6fe63ec
Mohammad Arasteh <mohammadkb79@gmail.com>
2024-03-05 01:07:23 +0330

    [chore] Set import path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@api": path.resolve(__dirname, "./src/api"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+      "@models": path.resolve(__dirname, "./src/models"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@store": path.resolve(__dirname, "./src/store"),
+      "@providers": path.resolve(__dirname, "./src/providers"),
+    },
+  },
   plugins: [react()],
-})
+});
