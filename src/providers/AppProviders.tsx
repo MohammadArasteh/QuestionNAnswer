72823916be179821ad2b081da50f9f09dbd516a2
Mohammad Arasteh <mohammadkb79@gmail.com>
2024-03-05 01:07:26 +0330

    [feat][ui] Replace chakra-ui with mui

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,4 +1,5 @@
 import { RouterProvider } from "react-router-dom";
+import CssBaseline from "@mui/material/CssBaseline/CssBaseline";
 import { DirectionProvider } from "./RtlProvider";
 import { router } from "../lib";
 import { Provider as RTKProvider } from "react-redux";
@@ -7,8 +8,9 @@ import { store } from "@/store/store";
 export function AppProviders() {
   return (
     <RTKProvider store={store}>
+      <CssBaseline />
       <DirectionProvider dir="rtl">
-        <RouterProvider router={router} fallbackElement={<div />} />
+        <RouterProvider router={router} />
       </DirectionProvider>
     </RTKProvider>
   );
